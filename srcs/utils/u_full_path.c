1e19c08482d3ed4794e1eece3b8b9b7ab0df519b
Iipal <malkoleyplay@gmail.com>
2019-11-06 19:00:37 +0200

    fixes

--- srcs/utils/u_full_path.c
+++ srcs/utils/u_full_path.c
@@ -6,15 +6,15 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/06 18:35:36 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/11/06 18:38:28 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/11/06 18:59:17 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ls.h"
 
-char	*u_full_path(char *register dst,
-			const char *register dir_path,
-			const char *register file)
+char	*u_full_path(char *restrict dst,
+			const char *restrict dir_path,
+			const char *restrict file)
 {
 	register size_t	dir_path_len = ft_strlen(dir_path);
 
