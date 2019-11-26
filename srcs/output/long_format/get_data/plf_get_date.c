ce668cc785a75d73f59e7bb877981fcbcac87d5a
Iipal <malkoleyplay@gmail.com>
2019-11-26 17:06:19 +0200

    correct checks date and getting year

--- srcs/output/long_format/get_data/plf_get_date.c
+++ srcs/output/long_format/get_data/plf_get_date.c
@@ -6,41 +6,23 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/28 22:17:07 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/11/26 16:26:26 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/11/26 17:02:43 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ls.h"
 
-static inline int __attribute__((__const__))
-	s_get_mon(const time_t file_time)
-{
-	return (localtime(&file_time)->tm_mon);
-}
-
-/*
-** date == "Sat Nov 23 12:54:51 2019\n".
-** When calling ft_strncpy:
-** date + 4 == "Nov 23 12:54:51 2019\n".
-** setting third param to 12 to copy only "Nov 23 12:54".
-**
-** If file_time is older more than 6 months or more from now
-**  - replacing time in string with a year from the end of full-date-string.
-*/
-
 inline char
 	*plf_get_date(char *restrict dst, const struct stat *restrict st)
 {
-	const int	mon_diff = s_get_mon(time(NULL)) - s_get_mon(st->st_ctime);
+	const int	mon_diff = time(NULL) - st->st_mtime;
 	char		*date_str;
 
 	date_str = ctime(IS_BIT(g_flags, BIT_U_ACCESS)
 			? &st->st_atime : &st->st_mtime);
 	dst = ft_strncpy(dst, date_str + 4UL, 12UL);
-	if (6 <= mon_diff || 0 > mon_diff)
+	if (TIME_HALF_YEAR <= mon_diff || 0 > mon_diff)
 	{
-		date_str = ctime(&st->st_ctime);
-		dst = ft_strncpy(dst, date_str + 4UL, 12UL);
 		dst[7] = ' ';
 		ft_strncpy(dst + 8UL, date_str + 20UL, 4UL);
 	}
