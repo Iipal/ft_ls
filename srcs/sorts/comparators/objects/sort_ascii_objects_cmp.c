7a6499b188c42d53da86d930a3eec51db11e5746
Iipal <malkoleyplay@gmail.com>
2019-11-30 12:57:51 +0200

    fixed sorts comparators

--- srcs/sorts/comparators/objects/sort_ascii_objects_cmp.c
+++ srcs/sorts/comparators/objects/sort_ascii_objects_cmp.c
@@ -6,7 +6,7 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/19 14:38:14 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/11/29 21:20:20 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/11/30 12:10:22 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,9 @@
 
 int32_t	sort_ascii_objects_cmp(const void *a, const void *b)
 {
-	const char		*a_str = ((const struct s_object*)a)->d_name;
-	const char		*b_str = ((const struct s_object*)b)->d_name;
-	const size_t	a_len = ((const struct s_object*)a)->d_name_len;
-	int32_t			cmp;
+	int32_t	cmp;
 
-	cmp = ft_strncmp(a, b, a_len);
+	cmp = ft_strcmp(((const struct s_object*)a)->d_name,
+					((const struct s_object*)b)->d_name);
 	return (IS_BIT(g_flags, BIT_R_SORT_REV) ? -cmp : cmp);
 }
--- srcs/sorts/comparators/objects/sort_time_objects_cmp.c
+++ srcs/sorts/comparators/objects/sort_time_objects_cmp.c
@@ -6,7 +6,7 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/31 16:29:03 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/11/29 13:02:39 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/11/30 11:58:08 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 int32_t	sort_time_objects_cmp(const void *a, const void *b)
 {
-	const struct s_object	*aa = (const struct s_object*)a;
-	const struct s_object	*bb = (const struct s_object*)b;
+	const struct s_stat	*st_a = ((const struct s_object*)a)->st;
+	const struct s_stat	*st_b = ((const struct s_object*)b)->st;
 	t_time_t				cmp;
 
 	if (IS_BIT(g_flags, BIT_U_ACCESS))
-		cmp = aa->st->a_time - bb->st->a_time;
+		cmp = st_a->a_time - st_b->a_time;
 	else
-		cmp = aa->st->c_time - bb->st->c_time;
+		cmp = st_a->c_time - st_b->c_time;
 	if (!cmp)
 		return (sort_ascii_objects_cmp(a, b));
 	return ((int32_t)(IS_BIT(g_flags, BIT_R_SORT_REV) ? cmp : -cmp));
